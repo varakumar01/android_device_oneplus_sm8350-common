bdd810ca8b7b72c52e3227130e9a8ba3547afd5c
basamaryan <basam.aryan@gmail.com>
2025-12-27 07:16:46 +0100

    sm8350-common: Inherit vendor/oneplus/dolby
    From: https://gitlab.com/NoPrincessHere/proprietary_vendor_oneplus_dolby
    
    Change-Id: I26fae5c3f0ab62445e628257943d7b6a48a2a478
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- extras.mk
+++ extras.mk
@@ -5,4 +5,4 @@
 #$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # Dolby 
-#$(call inherit-product, hardware/oplus/dolby/dolby.mk)
+$(call inherit-product, vendor/oneplus/dolby/oplusdolby.mk)
