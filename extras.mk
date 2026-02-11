650780bf1d5450435747bb555b8fea26599b3290
Jammy555 <prathap.venkata@gmail.com>
2026-02-11 17:15:10 +0530

    sm8350-common: Implement torchlight control for OP9
    ref: yaap/device_xiaomi_sunny@7486964sm8350-common:Implement torchlight
    Change-Id: Ia4939863cb4dc9af8a459b3af6c3cb3e6512e7e1

--- extras.mk
+++ extras.mk
@@ -1,6 +1,9 @@
 # DeviceAsWebcam
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
+# Torch
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.oneplus_sm8350)
+
 # OnePlus OOS Camera
 #$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
