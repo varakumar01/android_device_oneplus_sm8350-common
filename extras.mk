e7c93292859bdaae6e210709b4fb4ffeda41fd6f
Jammy555 <prathap.venkata@gmail.com>
2026-02-11 17:08:16 +0530

    [camera] enable the OOSCam inherit-product hook
    extras.mk
    
    Uncomments the vendor/oplus/camera/opluscamera.mk inherit-product-if-exists
    call. Aperture stays in PRODUCT_PACKAGES (common.mk) alongside it -- OOSCam
    is an additional camera app here, not a replacement.
    
    Change-Id: I4073eda30ccff053b4888bb4b5b508d510d4cc19
    
    Signed-off-by: Varakumar <varakumar7000@gmail.com>

--- extras.mk
+++ extras.mk
@@ -5,7 +5,7 @@ TARGET_BUILD_DEVICE_AS_WEBCAM := true
 $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.oneplus_sm8350)
 
 # OnePlus OOS Camera
-#$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # Dolby 
 $(call inherit-product, vendor/sony/dolby/sonydolby.mk)
